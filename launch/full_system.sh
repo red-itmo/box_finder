3107499e6a67d877149da20625894e63e60fea70
Evgeniy Antonov <antonovspostbox@gmail.com>
2016-07-22 19:48:03 +0300

    Adapt launches and configuration files for Stage

--- launch/full_system.sh
+++ launch/full_system.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 export ROSCONSOLE_FORMAT='[${severity}] [${node}]: ${message}'
-mkdir `rospack find box_finder`/map
+gnome-terminal -e "bash -c \"roscore ; exec bash\"" &
+sleep 2
+gnome-terminal -e "bash -c \"rosparam set use_sim_time true ; rosrun stage_ros stageros `rospack find box_finder`/map/debug.world ; exec bash\"" &
+sleep 2
 roslaunch box_finder automapping.launch
 roslaunch box_finder move_to_table.launch
